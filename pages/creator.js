89fb6a7e68e0f3ec1201b8350494b872a015c9ad
joshbm-git <joshbm.dev@gmail.com>
2020-05-27 12:42:53 +0200

    v2

--- pages/creator.js
+++ pages/creator.js
@@ -6,39 +6,48 @@ export default class CreatorPage {
   template() {
     document.querySelector("#app").innerHTML += /*html*/ `
         <section id="creator" class="page">
+        <div class="profileWrapper">
         <h2>Magnus Lygdback</h2>
-        <section class="grid">
-        <article class="left">
-        <article>
-        <img src="https://www.thegate.ca/wp-content/uploads/2020/02/Magnus-880x573.jpg">
-        <div class="button-wrapper">
-        <div class="btn-round">Subscribe</div>
-        <div class="btn-square"><i class="fab fa-facebook-messenger"></i> <p>Message</p></div>
-        <div class="btn-square"><i class="fab fa-instagram"></i><p>Share</p></div>
-        </div>
-        </article>
-        </article>
+          <div class="profileFlexWrapper">
+            <section class="profileMetadata">
+            <img src="https://www.thegate.ca/wp-content/uploads/2020/02/Magnus-880x573.jpg" class="profilePicture" alt="Playbook Profile Picture">
+              <a href="#myProfile" class="gradientButtonBlue">Subscribe</a>
+              <a href="#myProfile" class="strokedButton">Message</a>
+              <a href="#myProfile" class="strokedButton">Share</a>
+            </section>
+            
+            <section class="profileInformation">
+            <nav>
+            <ul>
+            <li>About</li>
+            <li class="active">Workouts</li>
+            <li>Insights</li>
+            </ul>
+            </nav>
 
-        <article class="right">
-        <nav>
-        <ul>
-        <li>About</li>
-        <li class="active">Workouts</li>
-        <li>Insights</li>
-        </ul>
-        </nav>
+            <section class="right-content about">
+            
+            </section>
 
-        <section class="right-content workouts">
-        <img src="https://www.thegate.ca/wp-content/uploads/2020/02/Magnus-880x573.jpg">
-        <img src="https://www.thegate.ca/wp-content/uploads/2020/02/Magnus-880x573.jpg">
-        <img src="https://www.thegate.ca/wp-content/uploads/2020/02/Magnus-880x573.jpg">
-        <img src="https://www.thegate.ca/wp-content/uploads/2020/02/Magnus-880x573.jpg">
-        <img src="https://www.thegate.ca/wp-content/uploads/2020/02/Magnus-880x573.jpg">
-        </section>
-
-        
-        </article>
-        </section>
+            <section class="right-content workouts">
+            <img src="https://www.thegate.ca/wp-content/uploads/2020/02/Magnus-880x573.jpg" class="profilePicture">
+            <img src="https://www.thegate.ca/wp-content/uploads/2020/02/Magnus-880x573.jpg" class="profilePicture">
+            <img src="https://www.thegate.ca/wp-content/uploads/2020/02/Magnus-880x573.jpg" class="profilePicture">
+            <img src="https://www.thegate.ca/wp-content/uploads/2020/02/Magnus-880x573.jpg" class="profilePicture">
+            <img src="https://www.thegate.ca/wp-content/uploads/2020/02/Magnus-880x573.jpg" class="profilePicture">
+            </section>
+    
+            <section class="right-content insights">
+            <img src="https://www.thegate.ca/wp-content/uploads/2020/02/Magnus-880x573.jpg" class="profilePicture">
+            <img src="https://www.thegate.ca/wp-content/uploads/2020/02/Magnus-880x573.jpg" class="profilePicture">
+            <img src="https://www.thegate.ca/wp-content/uploads/2020/02/Magnus-880x573.jpg" class="profilePicture">
+            <img src="https://www.thegate.ca/wp-content/uploads/2020/02/Magnus-880x573.jpg" class="profilePicture">
+            <img src="https://www.thegate.ca/wp-content/uploads/2020/02/Magnus-880x573.jpg" class="profilePicture">
+            </section>
+    
+            </section>
+          </div>
+      </div>
         </section>
       `;
   }

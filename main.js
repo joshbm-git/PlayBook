1ba23b634feb169ccb21b0b01bd7ce0f15338dfe
Sander <sandernykjaer@gmail.com>
2020-06-01 10:38:50 +0200

    done

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
 // import your components, pages and services
 import NavBar from "./components/navbar.js";
 import HomePage from "./pages/home.js";
-import PersonsPage from "./pages/persons.js";
+import WorkoutPage from "./pages/workouts.js";
 import spaService from "./services/spa.js";
 
 // Declare and init
 let navbar = new NavBar();
 let homePage = new HomePage();
-let personsPage = new PersonsPage();
+let workoutPage = new WorkoutPage();
 
 // init services
 spaService.init();
 
 window.pageChange = () => spaService.pageChange();
+
+window.changeWorkout = (workouts, workoutNumber) =>
+  workoutPage.changeWorkout(workouts, workoutNumber);
+window.next = () => workoutPage.next();
+window.previous = () => workoutPage.previous();
+window.progress = () => workoutPage.progress();

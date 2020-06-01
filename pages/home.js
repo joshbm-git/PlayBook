1a53a24a3f72b3e86335edd9601f64b9bb3acf45
joshbm-git <joshbm.dev@gmail.com>
2020-06-01 22:38:02 +0200

    Update home.js

--- pages/home.js
+++ pages/home.js
@@ -20,17 +20,7 @@ export default class HomePage {
       <h1>My Creators</h1>
       <div class="flex-container">
         
-        
-        <div class="grid-creators">
-        <img src="images/ninafitness.png">
-        <h2>Nina Agdal</h2>
-        <a href="#creator"> <p>START</p> </a>
-        </div>
-        <div class="grid-creators">
-        <img src="images/ninafitness.png">
-        <h2>Nina Agdal</h2>
-        <a href="#creator"> <p>START</p> </a>
-        </div>
+
         <div class="grid-creators">
         <img src="images/ninafitness.png">
         <h2>Nina Agdal</h2>
@@ -50,34 +40,8 @@ export default class HomePage {
         <div class="flex-container" id="workouts">
         </div>
 
-        <h3>NUTRITION PLANS</h3>
-        <div class="flex-container">
-        <div class="grid-nutrition">
-        <img src="images/nutrition.jpg">
-        <h2>Super Hero</h2>
-        <p>Nutrition Plan</p>
-        </div>
-        
-        <div class="grid-nutrition">
-        <img src="images/nutrition.jpg">
-        <h2>Super Hero</h2>
-        <p>Nutrition Plan</p>
-        </div>
-        <div class="grid-nutrition">
-        <img src="images/nutrition.jpg">
-        <h2>Super Hero</h2>
-        <p>Nutrition Plan</p>
-        </div>
-        <div class="grid-nutrition">
-        <img src="images/nutrition.jpg">
-        <h2>Super Hero</h2>
-        <p>Nutrition Plan</p>
-        </div>
-        <div class="grid-nutrition">
-        <img src="images/nutrition.jpg">
-        <h2>Super Hero</h2>
-        <p>Nutrition Plan</p>
-        </div>
+     
+       
         
         </div>
         </div>

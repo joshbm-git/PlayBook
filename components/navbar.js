a1ed26039f0efa060855ec4bc950abd68e52c3bd
Jacob <jacob-jaytee@hotmail.com>
2020-06-01 11:54:46 +0200

    fixed logo and background color

--- components/navbar.js
+++ components/navbar.js
@@ -7,10 +7,10 @@ export default class NavBar {
     document.querySelector("#app").innerHTML += /*html*/ `
       <nav class="tabbar">
 
-      <div class="logo"> <a href="#home">  <img src="../images/logo-pb.png" alt=""></a>
+      <div class="logo"> <a href="#home">  <img src="images/logo-pb.png" alt=""></a>
     </div>
-      
-      <div class="links">        
+
+      <div class="links">
       <a href="#home"><i class="fas fa-home"></i></a>
       <a href="#myProfile"><i class="fas fa-user"></i></a>
       <a href="#persons"><i class="fas fa-search"></i></a>

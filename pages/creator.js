6259a1b1788339fb66c64737026a772a340ac6cd
joshbm-git <joshbm.dev@gmail.com>
2020-06-01 11:28:58 +0200

    fixed creator

--- pages/creator.js
+++ pages/creator.js
@@ -10,7 +10,7 @@ export default class CreatorPage {
         <h2>Magnus Lygdback</h2>
           <div class="profileFlexWrapper">
             <section class="profileMetadata">
-            <img src="https://www.thegate.ca/wp-content/uploads/2020/02/Magnus-880x573.jpg" class="profilePicture" alt="Playbook Profile Picture">
+            <img src="../images/magnus.jpg" class="profilePicture" alt="Playbook Profile Picture">
               <a href="#myProfile" class="gradientButtonBlue">Subscribe</a>
               <a href="#myProfile" class="strokedButton">Message</a>
               <a href="#myProfile" class="strokedButton">Share</a>
@@ -27,46 +27,99 @@ export default class CreatorPage {
             
             <section class="hide right-content" id="contentAbout">
             <p>About</p>
-            <img src="https://www.thegate.ca/wp-content/uploads/2020/02/Magnus-880x573.jpg" class="profilePicture">
-            <img src="https://www.thegate.ca/wp-content/uploads/2020/02/Magnus-880x573.jpg" class="profilePicture">
-            <img src="https://www.thegate.ca/wp-content/uploads/2020/02/Magnus-880x573.jpg" class="profilePicture">
-            <img src="https://www.thegate.ca/wp-content/uploads/2020/02/Magnus-880x573.jpg" class="profilePicture">
-            <img src="https://www.thegate.ca/wp-content/uploads/2020/02/Magnus-880x573.jpg" class="profilePicture">
+            <h2>Trailer</h2>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/J6_bL-nUeAo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <h2>Story</h2>
+            <h3>A brief intro</h3>
+            <p>Magnus Lygdback is a world-renowned Swedish health and wellness expert, entrepreneur, and life coach. For over 18 years, he has become best known for working with some of the world's most recognizable actors, musicians, athletes, artists and high-performance business entrepreneurs.<br><br>Throughout his career, Magnus has developed the "Magnus Method", a training philosophy that embraces the "ethos of an athlete" - approaching life fitness with the same specific intensity and structure that an athlete would, whilst also maintaining a balanced and healthy life. Magnus' long list of clients includes Avicii, Max Martin, Alexander Skarsgård, Swedish House Mafia, Britney Spears, Mike Posner, Ben Affleck, Gal Gadot, Katy Perry, Lukas Graham, Harry Styles and Alicia Vikander to name a few.</p>
+            <h2>Goals</h2>
+            <h3>What I want us to accomplish</h3>
+            <p>Build muscle and aesthetics while toning up!</p>
+            <h2>Accomplishments</h2>
+            <h3>My certificates, accomplishments, etc.</h3>
+            <p>Certified trainer</p>
+            <h2>Requirements</h2>
+            <h3>What you need to train with me</h3>
+            <p>Gym membership</p>
+            <h2>Social Media</h2>
+            <p>Follow me on Instagram and remember to tag me in your pictures!</p>
+            <a href="https://www.instagram.com/magnuslygdback" target="_BLANK" class="gradientButtonBlue">Open Instagram</a>
+            <h2>Want to know more?</h2>
+            <p>Feel free to message me and i'll do my best to get back to you.</p>
+            <a href="#creator" class="gradientButtonBlue">Send message</a>
             </section>
 
             <section class="show right-content" id="contentWorkouts">
-            <p>Workouts</p>
+            <div class="content-flex">
+            <figure>
             <img src="https://www.thegate.ca/wp-content/uploads/2020/02/Magnus-880x573.jpg" class="profilePicture" onclick="collapseWorkout1()">
+            <p>Beginner Bodyweight</p>
+            </figure>
+            <figure>
             <img src="https://www.thegate.ca/wp-content/uploads/2020/02/Magnus-880x573.jpg" class="profilePicture" onclick="collapseWorkout2()">
+            <p>At home sculpt</p>
+            </figure>
+            <figure>
             <img src="https://www.thegate.ca/wp-content/uploads/2020/02/Magnus-880x573.jpg" class="profilePicture" onclick="collapseWorkout3()">
-
+            <p>Bodyweight</p>
+            </figure>
+            </div>
+            
+<div class="content-grid">
+<a href="#workouts" class="gradientButtonBlue">Start workout</a>
             <section id="workout1" class="show">
-            <p>Workout1</p>
-            <img src=" https://pbs.twimg.com/media/ERNBiC2WsAEoa3T.jpg" class="profilePicture">   
-            <img src=" https://pbs.twimg.com/media/ERNBiC2WsAEoa3T.jpg" class="profilePicture">   
-            <img src=" https://pbs.twimg.com/media/ERNBiC2WsAEoa3T.jpg" class="profilePicture">   
+            <figure>
+            <img src=" https://pbs.twimg.com/media/ERNBiC2WsAEoa3T.jpg">  
+            <p>Core Work</p>
+            </figure> 
+            <figure>
+            <img src=" https://pbs.twimg.com/media/ERNBiC2WsAEoa3T.jpg" >  
+            <p>Biceps</p>
+            </figure>  
+            <figure>
+            <img src=" https://pbs.twimg.com/media/ERNBiC2WsAEoa3T.jpg" >  
+            <p>Triceps</p>
+            </figure>  
             </section>
 
             <section id="workout2" class="hide">
-            <p>Workout2</p>
-            <img src=" https://pbs.twimg.com/media/ERNBiC2WsAEoa3T.jpg" class="profilePicture">   
-            <img src=" https://pbs.twimg.com/media/ERNBiC2WsAEoa3T.jpg" class="profilePicture">   
-            <img src=" https://pbs.twimg.com/media/ERNBiC2WsAEoa3T.jpg" class="profilePicture">   
+            <figure>
+            <img src=" https://pbs.twimg.com/media/ERNBiC2WsAEoa3T.jpg">  
+            <p>W1/D1 Bodyweight</p>
+            </figure> 
+            <figure>
+            <img src=" https://pbs.twimg.com/media/ERNBiC2WsAEoa3T.jpg" >  
+            <p>W1/D2 Bodyweight</p>
+            </figure>  
+            <figure>
+            <img src=" https://pbs.twimg.com/media/ERNBiC2WsAEoa3T.jpg" >  
+            <p>W1/D3 Bodyweight</p>
+            </figure>  
             </section>
 
             <section id="workout3" class="hide">
-            <p>Workout3</p>
-            <img src=" https://pbs.twimg.com/media/ERNBiC2WsAEoa3T.jpg" class="profilePicture">   
-            <img src=" https://pbs.twimg.com/media/ERNBiC2WsAEoa3T.jpg" class="profilePicture">   
-            <img src=" https://pbs.twimg.com/media/ERNBiC2WsAEoa3T.jpg" class="profilePicture">   
+            <figure>
+            <img src=" https://pbs.twimg.com/media/ERNBiC2WsAEoa3T.jpg" >  
+            <p>Hiit, Core, and lower body</p>
+            </figure> 
+            <figure>
+            <img src=" https://pbs.twimg.com/media/ERNBiC2WsAEoa3T.jpg">  
+            <p>Hiit, Core, and upper body</p>
+            </figure>  
+            <figure>
+            <img src=" https://pbs.twimg.com/media/ERNBiC2WsAEoa3T.jpg" >  
+            <p>Full body</p>
+            </figure>  
             </section>
-            </section>
+            </div>
+     
+        
 
             <section class="hide right-content" id="contentInsights">
             <p>Nutrition plan</p>
 
             </section>
-    
+            </section>
             </section>
           </div>
       </div>
